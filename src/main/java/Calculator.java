81d59166bb24635be8d60219999b2aa5152c9574
jo-emalo <37541560+jo-emalo@users.noreply.github.com>
2018-05-15 20:50:24 +0100

    no predefined numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,27 +1,18 @@
 public class Calculator {
-    public int num1;
-    public int num2;
+    
+       public int add(int num1, int num2){
+        return num1 + num2;
+    }
 
-    public Calculator(int num1, int num2){
-        this.num1 = num1;
-        this.num2 = num2;
+    public int subtract(int num1, int num2){
+        return num1 - num2;
     }
-    public int getNum1(){
-        return this.num1;
+
+    public int multiply(int num1, int num2){
+        return num1 * num2;
     }
-    public int getNum2(){
-        return this.num2;
-    }
-    public int getAddition(){
-        return (this.num1 + this.num2);
-    }
-    public int getSubtraction(){
-        return (this.num1 - this.num2);
-    }
-    public int getMultiply(){
-        return (this.num1 * this.num2);
-    }
-    public int getDivide(){
-        return (this.num1 / this.num2);
+
+    public double divide(int num1, int num2){
+        return num1 / num2;
     }
 }
